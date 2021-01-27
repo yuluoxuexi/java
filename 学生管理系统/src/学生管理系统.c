a09da2b4ea90566bbb9e6c475c6cda359a7b1ae3
HapiFive <1143910315@qq.com>
2021-01-27 18:03:29 +0800

    Update 学生管理系统.c

--- 学生管理系统/src/学生管理系统.c
+++ 学生管理系统/src/学生管理系统.c
@@ -1,7 +1,7 @@
 /*
  ============================================================================
  Name        : 学生管理系统.c
- Author      : 
+ Author      :
  Version     :
  Copyright   : Your copyright notice
  Description : Hello World in C, Ansi-style
@@ -18,7 +18,7 @@ struct sutdent {
 } student[100];
 int x = 0;
 void shurushuju() { //输入数据
-	FILE *fp = fopen("学生管理系统.txt", "a");
+	FILE* fp = fopen("学生管理系统.txt", "a");
 	printf("输入第%d个学生学号\n", x + 1);
 	fflush(stdout);
 	scanf("%d", &student[x].xuehao);
@@ -41,51 +41,51 @@ void shurushuju() { //输入数据
 	fclose(fp);
 }
 void shuchushuju() { //读取数据
-	FILE *fp = fopen("学生管理系统.txt", "r");
-	int b = 0; //打印学生数量
+	FILE* fp = fopen("学生管理系统.txt", "r");
 	if (fp != NULL) {
-		if(x==0){
+		if (x == 0) {
 			printf("无数据输出，清返回上一步输入1输入数据\n");
 		}
-		for (b = 0; b < x;b++) {
-			fread(&student[b], sizeof(student), 1, fp);
-			printf("学生学号%d，学生姓名%s，学生性别%s，学生成绩%d\n", student[b].xuehao,
-					student[b].name, student[b].sex, student[b].socer);
+		for (int i = 0; i < x; i++) {
+			fread(&student[i], sizeof(student), 1, fp);
+			printf("学生学号%d，学生姓名%s，学生性别%s，学生成绩%d\n", student[i].xuehao,
+				student[i].name, student[i].sex, student[i].socer);
 		}
 		fclose(fp);
-	} else {
+	}
+	else {
 		printf("找不到文件");
 	}
 }
-void xiugaishuju(){
+void xiugaishuju() {
 
 }
-void shanchushuju(){
-	FILE *fp=fopen("学生管理系统.txt", "w");
-	x=0;
-	fclose(fp);
+void shanchushuju() {
+	x = 0;
+	remove("学生管理系统.txt");
 	printf("删除成功,返回上一步操作\n");
 }
 void main() {
+	remove("学生管理系统.txt");
 	int i;
-	while(1){
+	while (1) {
 		printf("请输入数字进行操作：1 输入数据  2 输出数据  3修改数据  4删除数据  5查找数据  6退出\n");
 		fflush(stdout);
-		scanf("%d",&i);
+		scanf("%d", &i);
 		fflush(stdin);
-		if(i==1){
-	shurushuju();
+		if (i == 1) {
+			shurushuju();
 		}
-		if(i==2){
+		if (i == 2) {
 			shuchushuju();
 		}
-		if(i==3){
+		if (i == 3) {
 			xiugaishuju();
 		}
-		if(i==4){
+		if (i == 4) {
 			shanchushuju();
 		}
-		if(i==6){
+		if (i == 6) {
 			exit(0);
 		}
 	}
